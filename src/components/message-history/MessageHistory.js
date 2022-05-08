ec7ea4bb7e89b90111c5e8b00a0c08aaa8ec9241
Guy Ginton <guyginton@gmail.com>
2022-05-08 09:50:53 +0300

    add more details to frame

--- src/components/message-history/MessageHistory.js
+++ src/components/message-history/MessageHistory.js
@@ -2,17 +2,42 @@ import React from 'react';
 import PreviousMessage from '../previous-message/PreviousMessage';
 
 function MessageHistory() {
+    let elementEx1 = {
+        id: 1,
+        date: "Sunday, 17-May-20 11:18:45 UTC",
+        tel: "999-888-7777",
+        msg: "Hi! This is a message. I'm a fancy message that lives inside a box."
+    }
+    let elementEx2 = {
+        id: 2,
+        date: "Sunday, 17-May-20 11:18:45 UTC",
+        tel: "999-888-7777",
+        msg: "Hi! This is a second message. Due to time, I'll be repeating it and messing up the key/id vals"
+    }
+
+    let msgCountTotal = 23
+
+    // let showMSG =
+    //     element !== undefined &&
+    //     element !== null &&
+    //     element !== "";
+
     return (
         <div className="container">
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">Message History <span>(##)</span></h5>
-                    <PreviousMessage />
-                    <PreviousMessage />
-                    <PreviousMessage />
-                    <PreviousMessage />
-                    <PreviousMessage />
-                    <PreviousMessage />
+                    <h5 className="card-title">Message History <span>({msgCountTotal})</span></h5>
+                    {/* {showMSG && <PreviousMessage ...} */}
+                    <PreviousMessage
+                        element={elementEx1}
+                    // key={elementEx1.id}, date={elementEx1.date}, tel={elementEx1.tel}, msg={elementEx1.msg}
+                    />
+                    <PreviousMessage
+                        element={elementEx2}
+                    />
+                    <PreviousMessage
+                        element={elementEx2}
+                    />
                 </div>
             </div>
         </div>
--- src/components/previous-message/PreviousMessage.js
+++ src/components/previous-message/PreviousMessage.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-function PreviousMessage() {
+function PreviousMessage({ element }) {
+
     return (
         <div className="container">
             <div className="card">
 
                 <div className="prev-msg-header">
-                    <div className='bold'>555-555-5555</div>
-                    <div>Sunday, 17-May-20 11:18:45 UTC</div>
+                    <div className='bold'>{element.tel}</div>
+                    <div>{element.date}</div>
                 </div>
 
                 <div className='msg-content'>
                     <textarea
-                        className="form-control" 
+                        className="form-control"
                         id="msgFormInput"
-                        rows="3" 
-                        defaultValue={`Hi! This is a message. I'm a fancy message that lives inside a box.`}
+                        rows="3"
+                        defaultValue={element.msg}
                     />
                 </div>
 
